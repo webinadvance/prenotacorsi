f3322ec4d3fcbc2b5dbbb783e55524af35bd31d9
MatteoMaggiolini <webinadvance@gmail.com>
2023-03-07 17:15:41 +0100

    fdasfdsf

--- lib/widgets/LoginPage.dart
+++ lib/widgets/LoginPage.dart
@@ -9,7 +9,10 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId:
+        '586633050644-iu7iink9u7ci0us2iq7rajsqvd44ofm7.apps.googleusercontent.com',
+  );
 
   Future<UserCredential> _signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -35,8 +38,7 @@ class _LoginPageState extends State<LoginPage> {
           child: Text("Login with Google"),
           onPressed: () {
             _signInWithGoogle().then((UserCredential userCredential) {
-
-
+              print(userCredential);
             }).catchError((e) => print(e));
           },
         ),
